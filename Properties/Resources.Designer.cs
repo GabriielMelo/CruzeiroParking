c77d77341d58de6cd1e75814402a96742c245221
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2023-11-22 20:45:23 -0300

    Cruzeiro parking
    Em desenvolvimento.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PrototipoProjetoInterdisciplinar.Properties {
+namespace CruzeiroParking.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PrototipoProjetoInterdisciplinar.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PrototipoProjetoInterdisciplinar.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CruzeiroParking.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
